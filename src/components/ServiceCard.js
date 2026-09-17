f98df94419f7dffac8514b0afad82c128ccac0bb
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-17 23:15:58 +0300

    site: render service cards with approved imagery

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,12 +1,14 @@
 import { icon } from './icons.js';
+import { renderResponsiveImage } from './ResponsiveImage.js';
 
 export const renderServiceCard = (service) => `
 <a class="service-card service-card--${service.feature} service-card--${service.tone}" href="/services/${service.slug}/">
-  <div class="service-card__visual" aria-hidden="true">
-    <span class="service-card__halo"></span>
-    <span class="service-card__icon">${icon(service.icon)}</span>
+  <div class="service-card__visual">
+    ${renderResponsiveImage({ name: service.image, alt: '', className: 'service-card__image', variant: 'card' })}
+    <span class="service-card__veil" aria-hidden="true"></span>
+    <span class="service-card__icon" aria-hidden="true">${icon(service.icon)}</span>
     <span class="service-card__kicker">${service.kicker}</span>
-    <div class="service-card__chips">
+    <div class="service-card__chips" aria-hidden="true">
       ${service.examples.slice(0, 3).map((example) => `<span>${example}</span>`).join('')}
     </div>
   </div>
