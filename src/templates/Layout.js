be1529088ee78b5f751c1c2f4e53922bb5c3e8a1
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-18 00:46:31 +0300

    design: wire Cairo webfont in document head

--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -40,6 +40,9 @@ export const renderLayout = ({ title, description, currentPath = '/', content, n
   <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
   <link rel="apple-touch-icon" href="/assets/favicon/apple-touch-icon.png">
   <link rel="manifest" href="/site.webmanifest">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="/assets/site.css">
   <script type="application/ld+json">${organization}</script>
   <script src="/assets/site.js" defer></script>
