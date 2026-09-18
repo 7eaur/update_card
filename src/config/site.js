823cd65cc252fbc0dd582a7366d5f869d85959b4
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-19 01:14:46 +0300

    content: add official domain email and location

--- src/config/site.js
+++ src/config/site.js
@@ -2,10 +2,17 @@ export const site = {
   brandName: 'UPDATE CARD',
   brandNameAr: 'أبديت كارد',
   since: 2018,
+  siteUrl: 'https://updatecard.net',
+  domainDisplay: 'updatecard.net',
+  email: 'updatecardye@gmail.com',
+  emailHref: 'mailto:updatecardye@gmail.com',
+  location: 'اليمن - صنعاء',
   phoneDisplay: '770498884',
   phoneHref: 'tel:+967770498884',
   whatsappHref: 'https://wa.me/967770498884',
   facebookHref: 'https://www.facebook.com/Update.Cards.770498884/',
+  developerNameAr: 'وصل تك',
+  developerUrl: 'https://www.wasl-tech.com',
   description:
     'أبديت كارد جهة متخصصة في الخدمات الرقمية وحلول الشحن والدفع والشراء الإلكتروني منذ عام 2018، وتخدم عملاء التجزئة والجملة عبر مجموعة واسعة من الخدمات والمنصات المدعومة.',
 };
